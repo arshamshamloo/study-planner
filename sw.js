6ade8c206223a03be6aa829d6c1d7b6310fd46dc
arshamshamloo <arshamshamlw603@gmail.com>
2026-09-19 11:38:18 +0330

    Add chapter/subject fields to quick picker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* cache-first service worker for the study planner */
-var CACHE = 'gam-planner-v7';
+var CACHE = 'gam-planner-v8';
 var ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', function(e){
   e.waitUntil(
